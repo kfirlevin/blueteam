1cdf16d5e43ad5882d9ace4629ec0bbe05ba998c
Yanay <yanaymalik22@gmail.com>
2019-09-01 17:00:53 +0300

    fix health func

--- devops/health.py
+++ devops/health.py
@@ -1,22 +1,22 @@
 #!/bin/python
 import requests
 from datetime import datetime 
-weight = 'https://api.github.com'
-provider = 'https://api.github.com'
+weight = 'http://localhost:8090/health'
+provider = 'http://localhost:8080/health'
 
 weightResponse = requests.get(weight)
 providerResponse = requests.get(provider)
 
-if weightResponse.status_code != 200:
+if weightResponse.status_code == 200:
     print('Success!')
-elif weightResponse.status_code != 500:
+elif weightResponse.status_code == 500:
     fh = open("weightLog.txt","a+") 
     fh.write("[{}] [{}]\n".format(datetime.now(), 'internal server error'))
     fh.close() 
     
-if providerResponse.status_code != 200:
+if providerResponse.status_code == 200:
     print('Success!')
-elif providerResponse.status_code != 500:
-    fh = open("providerLog.txt","a") 
+elif providerResponse.status_code == 500:
+    fh = open("providerLog.txt","a+") 
     fh.write("[{}] [{}]\n".format(datetime.now(),'internal server error'))
     fh.close() 

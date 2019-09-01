8bf0dcadfd7d6ae38b68c953a925cd6163c0f670
awedz <kiril.g88@gmail.com>
2019-08-31 23:52:30 -0400

    debug logger

--- weight/config/logger.py
+++ weight/config/logger.py
@@ -1,12 +1,51 @@
 import logging
 import datetime
-#logging.basicConfig(filename='test.log',filemode='w',format=f'{str(datetime.datetime.now())[:19]}')
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter('[%(asctime)s] user : %(name)s level : %(levelname)s : %(message)s')
 
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+class Logger:
+    formatter = logging.Formatter('[%(asctime)s] caller : %(name)s | level - %(levelname)s : %(message)s')
+    def __init__(self):
+        pass
 
-logger.debug('often makes a very good meal of %s', 'test')
+class DebugLog(Logger):
+    def __init__(self,caller):
+        self.logger = logging.getLogger(caller)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.Debuglogger = logging.FileHandler('../logs/DEBUG.log')
+        self.Debuglogger.setLevel(logging.DEBUG)
+
+        self.formatter = Logger.formatter
+        self.Debuglogger.setFormatter(self.formatter)
+
+        self.logger.addHandler(self.Debuglogger)
+
+    def log(self,msg):
+        print(msg)
+        self.logger.debug(msg)
+
+        
+a = DebugLog('some api')
+a.log('test2')
+
+
+#logger = logging.getLogger('spam_application')
+#logger.setLevel(logging.DEBUG)#
+
+#fh = logging.FileHandler('spam.log')
+#fh.setLevel(logging.DEBUG)
+
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#fh.setFormatter(formatter)
+
+#logger.addHandler(fh)
+
+
+#logger.info('creating an instance of auxiliary_module.Auxiliary')
+
+#logger.info('created an instance of auxiliary_module.Auxiliary')
+#logger.info('calling auxiliary_module.Auxiliary.do_something')
+#logger.error('asdasd')
+#logger.info('finished auxiliary_module.Auxiliary.do_something')
+#logger.info('calling auxiliary_module.some_function()')
+
+#logger.info('done with auxiliary_module.some_function()')

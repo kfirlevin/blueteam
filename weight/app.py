7f40fa6de63e67be1b9473d19c265bbec6686d8c
mochecoh <mcbootcampdev@gmail.com>
2019-09-02 12:36:21 +0300

    add unknown api

--- weight/app.py
+++ weight/app.py
@@ -28,6 +28,8 @@ def health():
         return 'Failure', 500
     cnx.close()
     return 'OK', 200
+
+
 @app.route('/weight', methods=['POST'])
 def weight_post():
     direction = request.form.get('direction')   
@@ -36,7 +38,25 @@ def weight_post():
     else:
         return 'Not a valid direction' , 400
 
+@app.route('/unknown', methods=['GET'])
+def unknown_weights():
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        print(err)
+        return 'Failure', 500
     
+    list_of_unknown = []
+
+    sql_select_Query = "select * from containers_registered"
+    cursor = cnx.cursor()
+    cursor.execute(sql_select_Query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if  not str(row[1]).isdigit():
+            list_of_unknown.append(row[0])
+    return str(list_of_unknown)
 
 # @app.route('/')
     

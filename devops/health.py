512d3f60d80fabfa55dbac8a5e5eb5aa2b545d79
Yanay <yanaymalik22@gmail.com>
2019-09-02 15:03:58 +0300

    health update

--- devops/health.py
+++ devops/health.py
@@ -1,22 +1,30 @@
 #!/bin/python
-import requests
+from requests.exceptions import HTTPError
 from datetime import datetime 
-weight = 'http://localhost:8090/health'
-provider = 'http://localhost:8080/health'
+import requests
 
-weightResponse = requests.get(weight)
-providerResponse = requests.get(provider)
 
-if weightResponse.status_code == 200:
-    print('Success!')
-elif weightResponse.status_code == 500:
-    fh = open("weightLog.txt","a+") 
-    fh.write("[{}] [{}]\n".format(datetime.now(), 'internal server error'))
-    fh.close() 
-    
-if providerResponse.status_code == 200:
-    print('Success!')
-elif providerResponse.status_code == 500:
-    fh = open("providerLog.txt","a+") 
-    fh.write("[{}] [{}]\n".format(datetime.now(),'internal server error'))
-    fh.close() 
+def getApi(port , api):
+    try:
+        response = requests.get('http://localhost:' + port + api) 
+        response.raise_for_status()
+    except HTTPError as http_err:
+        #if api not found
+        with open("errorLog.txt","a+") as fh: 
+            fh.write("[{}] 'HTTP error occurred: {}'.format(http_err)\n".format(datetime.now(), http_err))
+        return False
+    except Exception as err:
+        with open("erGETrorLog.txt","a+") as fh: 
+            fh.write("[{}] 'Other error occurred: {}'.format(err)\n".format(datetime.now(), err))
+
+        return False
+    else:
+        return True
+
+def testAPI():
+    health1 = getApi('8010' , 'health') 
+    health2 = getApi('8020' , 'health')
+    #first checking health's api's
+    if health1 == False or health2 == False:
+        return False 
+    return True

a2454f727acd791dea423f61504274c7a3504374
mochecoh <mcbootcampdev@gmail.com>
2019-09-02 13:16:36 +0300

    small change after the merge

--- weight/app.py
+++ weight/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import mysql.connector
 from mysql.connector import errorcode
+import datetime
 
 app = Flask(__name__)
 
@@ -38,6 +39,50 @@ def weight_post():
     else:
         return 'Not a valid direction' , 400
 
+
+
+
+@app.route('/weight', methods=['GET'])
+def unknown_weights():
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        print(err)
+        return 'Failure', 500
+
+    
+    time_actual = datetime.datetime.now().strftime("%Y/%m/%d%I%M%S")
+    if request.args.get('t1')is None:
+        pass
+    else:
+        t1 = request.args.get('t1')
+    
+    if request.args.get('t2')is None:
+        pass
+    else:
+        t2 = request.args.get('t2')
+    
+    if request.args.get('f')is None:
+        f = "in,out,none"
+    else:
+        f = request.args.get('f')
+    
+    list_of_unknown = []
+
+    sql_select_Query = "select * from containers_registered"
+    cursor = cnx.cursor()
+    cursor.execute(sql_select_Query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if  not str(row[1]).isdigit():
+            list_of_unknown.append(row[0])
+    return str(time_actual)
+
+
+
+
+
 @app.route('/unknown', methods=['GET'])
 def unknown_weights():
     try:
@@ -57,6 +102,7 @@ def unknown_weights():
         if  not str(row[1]).isdigit():
             list_of_unknown.append(row[0])
     return str(list_of_unknown)
+
 
   
 @app.route('/batch_weight', methods=['POST'])

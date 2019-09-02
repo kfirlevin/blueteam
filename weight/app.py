2289c3c05741aedbf470d6f5cd5c3e361e379702
navelevi <navelevi@outlook.com>
2019-09-02 14:49:38 +0300

    added batch-weight

--- weight/app.py
+++ weight/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import mysql.connector
 from mysql.connector import errorcode
+import csv
 
 app = Flask(__name__)
 
@@ -39,28 +40,38 @@ def weight_post():
 
 
   
-@app.route('/batch_weight', methods=['POST'])
+@app.route('/batch-weight', methods=['POST'])
 def batch_weight():
+    fileName = request.form.get('file')
+    # file=open("in/"+fileName,'r')
+    # data=file.read()
+    # file.close()
+    spamReader = csv.reader(open('./in/'+fileName, newline=''), delimiter=',', quotechar='|')
+    #print(spamReader.headline)
+    ids=[]
+    weights=[]
+    for row in spamReader:
+        ids.append(row[0])
+        weights.append(row[1])
+        # print(row[1])
+        # print(type(row))
+        print(row)
+        #print(', '.join(row))
+    if str(weights[0]) == '"lbs"':
+        convert=True
+    else:
+        convert=False
+    weights.pop(0)
+    ids.pop(0)
+    if convert:
+        for i in range (len(weights)):
+            #item=(0.453592*float(item))
+            weights[i] = str(int(0.453592*float(weights[i])))
+    for i in range(len(ids)):
+        print("id:"+ids[i]+", weight: "+weights[i])
 
-
-
-    try:
-
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-
-
-
-        cnx = mysql.connector.connect(**config)
-
-
-
-    except mysql.connector.Error as err:
-        print(err)
-        return 'Failure', 500
-    cnx.close()
-    return 'OK', 200
-
+    return ("ok?")
+    
 
 
 if __name__ == '__main__':

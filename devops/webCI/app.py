0eb1ee86df86def2881ee787da719cb6256b954c
Igor <igrr69@gmail.com>
2019-09-02 16:14:08 +0300

    Add health.py to webci

--- devops/webCI/app.py
+++ devops/webCI/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, escape, request, json
 import auto_docker
+import health
+import time
+import sys
+
+sys.stdout.flush()
+
 app = Flask(__name__)
 
 repo_path = '/app/blueteam'
 
 auto_docker.update_repo(repo_path)
 auto_docker.activate_docker()
+time.sleep(30)
+if health.testAPI() == True:
+    print("BOKERRRR TOOOOVVV!!!!")
+else:
+    print('HARA!')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -19,6 +30,5 @@ def push():
         return "This is WebCI"
 
 
-#@app.route('/commit', methods=['POST', 'GET'])
-#def commitLog():
-    
+# @app.route('/commit', methods=['POST', 'GET'])
+# def commitLog():

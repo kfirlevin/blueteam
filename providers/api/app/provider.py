f67788c0f16b02646eeb76bae86ce1a9668f2533
Edi <edi.gamarnik@gmail.com>
2019-09-02 13:42:15 +0300

    POST PROVIDERS FEATURE DONE

--- providers/api/app/provider.py
+++ providers/api/app/provider.py
@@ -1,5 +1,40 @@
 from app import app
+from flask import Flask, render_template, redirect, request, make_response
+from urllib.parse import urlparse
+from typing import List, Dict
+import mysql.connector
+import json
+import os
+import logging
+import sys 
 
-@app.route('/provider')
+logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+
+def sql(value):
+    config = {
+    'user': "root",
+    'password': "root",
+    'host': "localhost",
+    'port': '5010',
+    'database': 'billdb'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"INSERT INTO Provider (`name`) VALUES ('{value}')")
+        cursor.execute(f"SELECT id from Provider WHERE name='{value}' ")
+        re=cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return re
+    except:
+        return "ERROR"
+
+@app.route('/provider', methods=["POST"])
 def provider():
-    return "provider"
+    value=request.args["provider_name"]
+    logging.info(f"INSERT {value} IN TO PROVIDER")
+    hh=sql(value)
+    if (hh != "ERROR"):
+        v1=hh[0]
+        return "{\"id\":\""+str(v1[-1])+"\"}"

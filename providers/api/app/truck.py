2f7f05f98ded64397834f07c0a1262e6cce26705
shai edri <shaiedri850@gmail.com>
2019-09-02 12:19:24 +0300

    Added truck POST

--- providers/api/app/truck.py
+++ providers/api/app/truck.py
@@ -1,5 +1,38 @@
 from app import app
+from flask import Flask, render_template, redirect, request
+from urllib.parse import urlparse
+from typing import List, Dict
+import mysql.connector
+import json
+import os
+from . import db
 
-@app.route('/truck')
-def truck():
-    return "truck"
+config = {
+'user': os.environ.get('DB_USER'),
+'password': os.environ.get('DB_PASS'),
+'host': os.environ.get('DB_HOST'),
+'port': '3306',
+'database': 'billdb'
+}
+
+def truckPost(truckId,providerId):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('''INSERT INTO Trucks (id,provider_id) VALUES (%s, %s);''', (truckId, providerId))
+    connection.commit()
+    cursor.execute('select * from Trucks')
+    results = cursor.fetchall()
+    dictionary = {}
+    for key, value in results:
+        dictionary[key] = value
+    cursor.close()
+    connection.close()
+    return dictionary
+
+
+@app.route('/truck',methods=['GET','POST','PUT'])
+def handleTruck():
+    if request.method == 'POST':
+        trcukId=request.args.get('id')
+        providerId=request.args.get('provider')
+        return json.dumps(truckPost(trcukId,providerId),sort_keys=True,indent=4)

e835159c42038556ec7f6d4084c32593db249b6e
Igor <igrr69@gmail.com>
2019-09-02 14:47:14 +0300

    Add CI server with docker-compose and dockerfile

--- devops/webCI/app.py
+++ devops/webCI/app.py
@@ -2,12 +2,18 @@ from flask import Flask, escape, request, json
 import auto_docker
 app = Flask(__name__)
 
+repo_path = '/app/blueteam'
+
+auto_docker.update_repo(repo_path)
+auto_docker.activate_docker()
+
 
 @app.route('/', methods=['POST', 'GET'])
 def push():
     if request.method == 'POST':
         if request.headers['Content-Type'] == 'application/json':
+            auto_docker.update_repo(repo_path)
             auto_docker.activate_docker()
             return json.dumps(request.json)
-    else if request.method == 'GET':
+    elif request.method == 'GET':
         return "This is WebCI"
--- devops/webCI/auto_docker.py
+++ devops/webCI/auto_docker.py
@@ -7,5 +7,13 @@ def activate_docker(environment='Testing'):
     #     cmd += '-f ../docker-prod/docker-compose.yml up -d'
     #     os.system(cmd)
     # else:
-    cmd += '-f ../docker-test/docker-compose.yml up -d'
+    cmd += '-f /app/blueteam/devops/docker-test/docker-compose.yml up'
     os.system(cmd)
+
+
+def update_repo(path):
+    if not os.path.isdir(path):
+        os.system(
+            'git clone https://github.com/tomikonio/blueteam.git {}'.format(path))
+    os.chdir(path)
+    os.system('git pull')

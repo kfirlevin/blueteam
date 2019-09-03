6bba2dade6d9155d5d1bf9f4f2ad9be72406f93b
shai edri <shaiedri850@gmail.com>
2019-09-03 13:52:06 +0300

    bill api truck mock

--- providers/api/app/bill.py
+++ providers/api/app/bill.py
@@ -1,5 +1,51 @@
 from app import app
+from flask import Flask, redirect, request
+from urllib.parse import urlparse
+from typing import List, Dict
+import mysql.connector
+import json
+import requests
+import os
+from . import db
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-@app.route('/bill')
-def bill():
-    return "bill"
+config = {
+'user': os.environ.get('DB_USER'),
+'password': os.environ.get('DB_PASS'),
+'host': os.environ.get('DB_HOST'),
+'port': '3306',
+'database': 'billdb'
+}
+
+#    Temporary mock
+@app.route('/sesseionMock/<id>', methods=['GET'])
+def sessionMOCK(id):
+    if request.method == 'GET':
+        jsonMock = {
+            "bruto": 1200, 
+            "containers": "Containers", 
+            "datetime": "Mon, 01 Jan 2001 01:01:01 GMT", 
+            "direction": "direction", 
+            "id": 1235123, 
+            "neto": 200, 
+            "produce": "produce", 
+            "truck": "truck", 
+            "truckTara": 1000
+            }
+    return json.dumps(jsonMock)
+
+@app.route('/bill/<id>',methods=['GET'])
+def handleBill(id):
+    if request.method == 'GET':
+        From=request.args.get('from')
+        To=request.args.get('to')
+        TruckInfo = json.loads(requests.get('http://localhost:5000/truck/{id}?from=11&to=12').content)
+        Bill = {
+            "id"   : TruckInfo["id"],
+            "name" : "name",
+            "from" : From,
+            "to"   : To
+        }
+    return Bill

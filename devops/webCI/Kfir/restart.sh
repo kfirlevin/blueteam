3f1b5af2874c30ffde1d4061f8e1184d20060ffd
Kfir Levin <kfir@gmail.com>
2019-09-03 13:15:31 +0300

    CI update

--- devops/webCI/Kfir/restart.sh
+++ devops/webCI/Kfir/restart.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+prod_names=( docker-prod_providers-db docker-prod_providers-server docker-prod_weight-db docker-prod_weight-server)
+test_names=( docker-test_providers-db docker-test_providers-server docker-test_weight-db docker-test_weight-server)
+
 pushd .
 cd /blueteam/devops/docker-test
-docker-compose up --build -d
+docker-compose up --build
+sleep 30
 
-## Testing 
-#status=$(python)
+## Testing
+docker-compose down
+for name in ${test_names[@]}
+do
+    docker rmi $name -f
+done 
+
 
 popd

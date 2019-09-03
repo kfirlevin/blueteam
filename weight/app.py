a62b509b68967ce0e67f85422527a29a347c010d
navelevi <navelevi@outlook.com>
2019-09-03 17:38:44 +0300

    weight_post basic template and beautifying code

--- weight/app.py
+++ weight/app.py
@@ -10,12 +10,12 @@ import os
 
 app = Flask(__name__)
 config = {
-  'user': 'db',
-  'password': 'password',
-  'host': os.environ.get('MYSQL_HOST'),
-  'port': '3306',
-  'database': 'weight',
-  'raise_on_warnings': True
+    'user': 'db',
+    'password': 'password',
+    'host': os.environ.get('MYSQL_HOST'),
+    'port': '3306',
+    'database': 'weight',
+    'raise_on_warnings': True
 }
 
 
@@ -41,60 +41,77 @@ def health():
 @app.route('/weight', methods=['POST'])
 def weight_post():
     if request.method == 'GET':
-            return render_template("weight-form.html")
+        return render_template("weight-form.html")
     elif request.method == 'POST':
-        id = request.form.get('id')
-        datetime = request.form.get('datetime')
+        force = False
         direction = request.form.get('direction')
+        if direction not in ("out", "in", "none"):
+            return 'Not a valid direction', 400
+        if direction == 'none'
+        direction = 'in'
         truck = request.form.get('truck')
-        containers = request.form.get('containers')
-        bruto = request.form.get('bruto')
-        truckTara = request.form.get('truckTara')
+        if truck is None:
+            return 'No truck id?', 400
+        datetime = datetime.datetime.now()
+        containers = request.form.get('containers').split(',')
+        if containers is None:
+            return 'There are no containers?', 400
+        weight = request.form.get('weight')
+        if weight is None:
+            return 'You forgot to enter weight', 400
+        unit = request.form.get('unit')
+        if unit not in ("kg", "lbs"):
+            return 'unit not supported', 400
+        if unit == 'lbs':
+            weight = int(0.453592*float(weight))
+            unit = 'kg'
         produce = request.form.get('produce')
-    if Connection.Mysql.isHealth() == True : 
+        if produce not in ("tomato", "orange"):
+            return 'Not a valid produce', 400
+        if request.form.get('fouce') == 'true':
+            force = True
+
+    if Connection.Mysql.isHealth() == True:
         return Weight.weight_post(direction)
     return "Error: DB Connection"
 
-    
+
 # Author:
 # TODO Add Comments - Description
 @app.route('/weight', methods=['GET'])
 def weights_get():
-    if Connection.Mysql.isHealth() == True : 
-        return Weight.weights_get(request.args.get('from'),request.args.get('to'),request.args.get('filter'))
+    if Connection.Mysql.isHealth() == True:
+        return Weight.weights_get(request.args.get('from'), request.args.get('to'), request.args.get('filter'))
     return "Error: DB Connection"
 
 
-    
 # Author:
 # TODO Add Comments - Description
 @app.route('/unknown', methods=['GET'])
 def unknown_weights():
-    if Connection.Mysql.isHealth() == True : 
+    if Connection.Mysql.isHealth() == True:
         return Weight.unknown_weights()
     return "Error: DB Connection"
 
 
 # Author:
 # TODO Add Comments - Description
-@app.route('/batch-weight',  methods = ['GET', 'POST'])
+@app.route('/batch-weight',  methods=['GET', 'POST'])
 def batch_weight():
     if request.method == 'GET':
         return render_template("betch-weight.html")
     elif request.method == 'POST':
-        if Connection.Mysql.isHealth() == True : 
+        if Connection.Mysql.isHealth() == True:
 
-            fileName = request.form.get('file')        
+            fileName = request.form.get('file')
             return Weight.batch_weight(fileName)
 
         return "Error: DB Connection"
-        
-
 
 
 # Author:
 # TODO Add Comments - Description
-#GET /weight?from=t1&to=t2&filter=f
+# GET /weight?from=t1&to=t2&filter=f
 @app.route('/all_con', methods=['GET'])
 def get_weight():
 
@@ -104,19 +121,19 @@ def get_weight():
     list_of_unknown = []
 
     for row in rows:
-         list_of_unknown.append(
-           {
-            "container_id": row[0],
-            "weight": row[1],
-            "unit": row[2]
+        list_of_unknown.append(
+            {
+                "container_id": row[0],
+                "weight": row[1],
+                "unit": row[2]
             }
-         )
+        )
     return jsonify(list_of_unknown)
 
 
 # Author:
 # TODO Add Comments - Description
-#GET /weight?from=t1&to=t2&filter=f
+# GET /weight?from=t1&to=t2&filter=f
 @app.route('/all_transactions', methods=['GET'])
 def get_transaction():
 
@@ -126,29 +143,30 @@ def get_transaction():
     list_of_unknown = []
 
     for row in rows:
-         list_of_unknown.append(
-             Transaction.transactionToJson(row)
-         )
+        list_of_unknown.append(
+            Transaction.transactionToJson(row)
+        )
     return jsonify(list_of_unknown)
 
 # Author:
 # TODO Add Comments - Description
-## GET /item/<id>?from=t1&to=t2
-@app.route('/item/<string:id_num>', methods=['GET']) # TODO
+# GET /item/<id>?from=t1&to=t2
+@app.route('/item/<string:id_num>', methods=['GET'])  # TODO
 def get_item(id_num):
-    if Connection.Mysql.isHealth() == True : 
-        return Item.get_items(request.args.get('from'),request.args.get('to'),id_num)
+    if Connection.Mysql.isHealth() == True:
+        return Item.get_items(request.args.get('from'), request.args.get('to'), id_num)
     return "Error: DB Connection"
 
 
-# Author: 
+# Author:
 # TODO Add Comments - Description
 #   GET /session/<id>
-@app.route('/session/<string:id_num>', methods=['GET']) # TODO
+@app.route('/session/<string:id_num>', methods=['GET'])  # TODO
 def get_session(id_num):
-    if Connection.Mysql.isHealth() == True : 
+    if Connection.Mysql.isHealth() == True:
         return Transaction.get_session(id_num)
     return "Error: DB Connection"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- weight/tests/test_health.py
+++ weight/tests/test_health.py
@@ -4,6 +4,7 @@ import pytest
 
 URI = "http://blue.develeap.com:8090"
 
+
 def test_get_heath():
     response = requests.get(URI + "/health")
     assert response.status_code == 200

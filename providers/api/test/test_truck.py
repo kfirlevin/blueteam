8f3ea31dee89cc0d57d522edf68669135509939e
Ofer Hamberg <hambergofer@gmail.com>
2019-09-03 15:05:44 +0300

    some truck testing

--- providers/api/test/test_truck.py
+++ providers/api/test/test_truck.py
@@ -1,5 +1,21 @@
 import requests
 import os
+import json
+import random
 URI = os.environ.get('URI')
 def test_truck_post_good():
-    return 0
+    req1 = requests.post(URI + 'provider?provider_name=test11')
+    assert req1.status_code == 200
+    json1 = json.loads(req1.content)
+    req = requests.post(URI + 'truck?provider='+json1['id']+'&id='+str(random.randint(1, 9999999)))
+    assert req.status_code == 200
+def test_truck_post_bad():
+    req = requests.post(URI + 'provider?provid_name=test21')
+    assert req.status_code != 200
+def test_truck_post_bad_2():
+    req = requests.post(URI + 'provider?provider_name=test21')
+    assert req.status_code == 200
+    json1 = json.loads(req.content)
+    requests.post(URI + 'truck?provider='+json1['id']+'&id=5')
+    req3 = requests.post(URI + 'truck?provider='+json1['id']+'&id=5')
+    assert req3.status_code != 200

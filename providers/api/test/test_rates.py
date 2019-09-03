0b2cc7718454f32796aee2b33b243c70ab1c3dce
Ofer Hamberg <hambergofer@gmail.com>
2019-09-03 11:59:22 +0300

    test rates

---
+++ providers/api/test/test_rates.py
@@ -0,0 +1,31 @@
+import requests
+import os
+URI = os.environ.get('URI')
+def test_rates_get_bad():
+    req = requests.get(URI + 'rates')
+    assert req.status_code == 404
+    if req.status_code == 404:
+        return 0
+    else:
+        return 1
+def test_rates_post_bad():
+    req = requests.post(URI + 'rates?file=no_such_file.xlsx')
+    assert req.status_code == 500
+    if req.status_code == 500:
+        return 0
+    else:
+        return 1
+def test_rates_post_good():
+    req = requests.post(URI + 'rates?file=rates.xlsx')
+    assert req.status_code == 200
+    if req.status_code == 200:
+        return 0
+    else:
+        return 1
+def test_rates_get_good():
+    req = requests.get(URI + 'rates')
+    assert req.status_code == 200
+    if req.status_code == 200:
+        return 0
+    else:
+        return 1

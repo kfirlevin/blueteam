5f0595d3b3f389d4f7292e93a7b9b2145a8d08d3
ig <igAhmad71@gmail.com>
2019-09-03 14:43:00 +0300

    fixed the function TransactionToJson

--- weight/classes/Transaction.py
+++ weight/classes/Transaction.py
@@ -4,6 +4,18 @@ import datetime
 
 class Transaction():
 
+    def transactionToJson(row): 
+            return {
+                "id": row[0] , 
+                "datetime": row[1],
+                "direction": row[2],
+                "truck": row[3],
+                "containers": row[4],
+                "bruto": row[5],
+                "truckTara": row[6],
+                "neto": row[7],
+                "produce": row[8] 
+                }
 
     def get_session(id_num):
 

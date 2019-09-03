9aa0711edce175c64ce90dfebe3e2863f4e521f2
mochecoh <mcbootcampdev@gmail.com>
2019-09-03 16:32:30 +0300

    add test files

---
+++ weight/tests/test_health.py
@@ -0,0 +1,9 @@
+import requests
+import pytest
+
+
+URI = "http://blue.develeap.com:8090"
+
+def test_get_heath():
+    response = requests.get(URI + "/health")
+    assert response.status_code == 200

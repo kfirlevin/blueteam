ebca02674c2344db835d7d3c9a5aa6519f68d773
Igor <igrr69@gmail.com>
2019-09-04 16:10:29 +0300

    Chnage app.py to pre addition of branches

--- devops/webCI/app.py
+++ devops/webCI/app.py
@@ -1,29 +1,19 @@
-from flask import Flask, request, json
+from flask import Flask, request
 import subprocess
 app = Flask(__name__)
 
-
 @app.route('/up')
 def up():
-    subprocess.Popen("./prod-up.sh")
-    return "Up"
-
+        subprocess.Popen("./prod-up.sh")
+        return "Up"
 
 @app.route('/down')
 def down():
-    subprocess.Popen("./prod-down.sh")
-    return "Down"
+        subprocess.Popen("./prod-down.sh")
+        return "Down"
 
 
 @app.route('/webhook', methods=['POST'])
 def run_testing_env():
-    info = json.dumps(request.json)
-    temp_branch = info[info.find("refs/heads")+10:info.find("refs/heads")+50]
-    branch = temp_branch[1:temp_branch.find("repo")-4]
-    if branch == "master":
-        subprocess.Popen("./test.sh")
-    elif branch == "providers":
-        subprocess.Popen("./test2.sh {}".format(branch))
-    elif branch == "weight":
-        subprocess.Popen("./test2.sh {}".format(branch))
-    return "Yes"
+     subprocess.Popen("./test.sh")
+     return "Yes"

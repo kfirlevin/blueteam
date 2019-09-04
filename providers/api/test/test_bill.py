142cfb2ba2368a4c9da83a56c5b456e122a51f37
Ofer Hamberg <hambergofer@gmail.com>
2019-09-04 16:36:07 +0300

    With her finger and her thumb

--- providers/api/test/test_bill.py
+++ providers/api/test/test_bill.py
@@ -2,7 +2,7 @@ import requests
 import os
 import json
 URI = os.environ.get('URI')
-def test_bill_get():#
+def test_bill_get():
     req = requests.get(URI + 'bill/5')
     print (req.content)
     assert req.status_code == 200

bf428fbf5f51f9ba1a6cc36b2a799f9bd75f84cd
Igor <igrr69@gmail.com>
2019-09-04 16:50:06 +0300

    add json parse

--- devops/webCI/app.py
+++ devops/webCI/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, json
 import subprocess
 app = Flask(__name__)
 
+
 @app.route('/up')
 def up():
-        subprocess.Popen("./prod-up.sh")
-        return "Up"
+    subprocess.Popen("./prod-up.sh")
+    return "Up"
+
 
 @app.route('/down')
 def down():
-        subprocess.Popen("./prod-down.sh")
-        return "Down"
+    subprocess.Popen("./prod-down.sh")
+    return "Down"
 
 
 @app.route('/webhook', methods=['POST'])
 def run_testing_env():
-     subprocess.Popen("./test.sh")
-     return "Yes"
+    shuki = request.get_json()
+    print shuki
+    print shuki['ref'].split('/')[2]
+    if shuki['ref'].split('/')[2] == "providers":
+        subprocess.Popen("./test.sh")
+    return "Yes"

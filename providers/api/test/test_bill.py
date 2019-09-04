d7fd1ffe8ba5c5d846e0bfcfd616a9d323136487
Ofer Hamberg <hambergofer@gmail.com>
2019-09-04 16:13:21 +0300

    THis is commit #13 for debops

--- providers/api/test/test_bill.py
+++ providers/api/test/test_bill.py
@@ -3,6 +3,6 @@ import os
 import json
 URI = os.environ.get('URI')
 def test_bill_get():
-    req = requests.get(URI + 'bill/')
+    req = requests.get(URI + 'bill/5')
     print (req.content)
     assert req.status_code == 200

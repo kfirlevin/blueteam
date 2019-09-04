790b3ed41a0bf071dd3d9ff069e9a3a9c9192a2d
Ofer Hamberg <hambergofer@gmail.com>
2019-09-04 16:40:45 +0300

    !!

--- providers/api/test/test_bill.py
+++ providers/api/test/test_bill.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import json
-URI = os.environ.get('URI')#
+URI = os.environ.get('URI')
 def test_bill_get():
     req = requests.get(URI + 'bill/5')
     print (req.content)

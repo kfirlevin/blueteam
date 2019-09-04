e66c07bdd52b4cdd50d6b497c221f53adcd9050a
Igor <igrr69@gmail.com>
2019-09-04 15:11:38 +0300

    Add test to differnt branch pushes

--- devops/webCI/test2.sh
+++ devops/webCI/test2.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-prod_names=( prov-db-prod docker-prod_providers-server weight-db-prod docker-prod_weight-server)
 test_names=( prov-db-test docker-test_providers-server weight-db-test docker-test_weight-server)
 
 pushd .
 cd /blueteam/devops/docker-test
-git checkout master
+git checkout $1
 git pull
 docker-compose up --build -d
-
-## Testing
 
 ## Check if test servers are up
 counter=0
@@ -28,31 +24,12 @@ while [[ "$urls_up" == false && $counter -ne 120 ]]; do
     sleep 1
     counter=$((counter+1))
 done
-##########################
-status=$(python /app/Tests/e2e.py)
-if [ $status == "True" ]
-then
-    docker-compose down
-    for name in ${test_names[@]}
-    do
-        docker rmi $name -f
-    done
 
-    cd /blueteam/devops/docker-prod
-    docker-compose down
-    for name in ${prod_names[@]}
-    do
-        docker rmi $name -f
-    done
-    docker-compose up --build -d
-else
-    docker-compose down
-    for name in ${test_names[@]}
-    do
-        docker rmi $name -f
-    done
-fi
+status=$(python /app/Tests/e2e.py)
+docker-compose down
+for name in ${test_names[@]}
+do
+    docker rmi $name -f
+done
 docker volume prune -f
 docker images -qf dangling=true | xargs docker rmi
-
-popd

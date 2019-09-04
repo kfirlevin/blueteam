3e5853467ad0d90f4a0f142b63cbcb1f029c62bf
Igor <igrr69@gmail.com>
2019-09-04 14:33:02 +0300

    Add new url check before running tests

--- devops/webCI/test.sh
+++ devops/webCI/test.sh
@@ -8,6 +8,26 @@ git pull
 docker-compose up --build -d
 
 ## Testing
+
+## Check if test servers are up
+counter=0
+urls_up=false
+url1="http://blue.develeap.com:8081/health"
+url2="http://blue.develeap.com:8082/health"
+while [[ "$urls_up" == false && $counter -ne 120 ]]; do 
+    if curl --output /dev/null --silent --head --fail "$url1"; then
+        if curl --output /dev/null --silent --head --fail "$url2"; then
+            urls_up=true
+        else
+            urls_up=false
+        fi
+    else
+        urls_up=false
+    fi
+    sleep 1
+    counter=$((counter+1))
+done
+##########################
 status=$(python /app/Tests/e2e.py)
 if [ $status == "True" ]
 then

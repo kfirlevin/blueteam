f7e194a97c5425caf10b1d7fbbec582a6ce5cd65
Ofer Hamberg <hambergofer@gmail.com>
2019-09-04 15:58:35 +0300

    QQ

--- providers/api/test/test_bill.py
+++ providers/api/test/test_bill.py
@@ -3,6 +3,6 @@ import os
 import json
 URI = os.environ.get('URI')
 def test_bill_get():
-    req = requests.get(URI + 'bill/2')
+    req = requests.get(URI + 'bill/')
     print (req.content)
     assert req.status_code == 200

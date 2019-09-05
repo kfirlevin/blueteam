c3d5716cf9a3c811ffa12dfe4471771d05e6640c
Igor <igrr69@gmail.com>
2019-09-05 11:12:31 +0300

    Remove '/' from url ffsk

--- devops/webCI/Tests/e2e.py
+++ devops/webCI/Tests/e2e.py
@@ -8,16 +8,15 @@ import datetime
 
 def testresult(providers, weight):
     if providers == 0 and weight == 0:
-        telebot.sendMessage("Push was tested successfully - {}".format(
-            datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+        telebot.parse_files('providers.txt', 'weight.txt')
         return "True"
     else:
         telebot.parse_files('providers.txt', 'weight.txt')
         return "False"
 
 
-# providers = os.system(
-#     'export URI=http://blue.develeap.com:8081/ && pytest ../../providers > providers.txt')
+providers = os.system(
+    'export URI=http://blue.develeap.com:8081/ && pytest ../../providers > providers.txt')
 weight = os.system(
-    'export URI=http://blue.develeap.com:8082/ && pytest ../../weight > weight.txt')
-print(testresult(0, weight))
+    'export URI=http://blue.develeap.com:8082 && pytest ../../weight > weight.txt')
+print(testresult(providers, weight))
